9bf804d8c1648d686f43d2f74de57ecf1751836d
Unknown <pablo.osorio@xmarts.com>
2018-09-11 15:47:27 -0500

    add files

---
+++ powergy_reports/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Reports & Fields',
+    'version': '1.0',
+    'summary': 'Personalizacion de reportes y vistas para Powergy',
+    'description': 'Adecuaciones a los reportes incluidos en odoo, se incluyen campos a diversas ventanas para el uso de datos en los reportes que en este modulo se agregan',
+    'category': 'Personalizacion',
+    'author': 'William Colin Macedo',
+    'website': 'www.xmarts.com',
+    'depends': ['base','purchase','contacts'],
+    'data': ['views/view.xml',
+    		 'reports/report_purchase_order.xml',
+             'reports/report_factura.xml',
+             'reports/report_delivery_split.xml',
+             'reports/report_payment_receipt.xml',
+             'reports/report_saleorder.xml',             
+             'reports/report_albaran.xml' ],
+
+
+}
---
+++ powergy_reports/models/powergy_model.py
@@ -0,0 +1,66 @@
+from odoo import api, _, tools, fields, models, exceptions
+from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+from datetime import datetime
+from . import amount_to_text
+
+
+class ReportPurchase(models.Model):
+
+	_inherit = 'purchase.order'
+
+	type_order = fields.Selection([('national','National'),
+							   		('international','International')], help='Choose the type of order', default='national',required=True)
+	observations= fields.Text(string='Observations')
+	amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True,
+                                 help='Amount of the invoice in letter')
+	
+	@api.one
+	@api.depends('amount_total')
+	def _get_amount_to_text(self):
+		self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount_total)
+
+
+class ReportFactura(models.Model):
+
+	_inherit = 'account.invoice'
+
+	observations= fields.Text(string='Observations')
+
+
+class ReportPago(models.Model):
+
+	_inherit = 'account.payment'
+
+	cambio = fields.Float(string='Tipo Cambio')
+	bank_reference = fields.Char(string='Ref. Bancaria')
+
+
+class QuotationsFields(models.Model):
+
+	_inherit = 'sale.order'
+
+	indiv_orders = fields.Text(string='Pedidos Individuales')	
+	name_site = fields.Char(string='Nombre de sitio')
+	no_adrisa = fields.Char(string='No. Adrisa/Grafo')
+	folio_patrimonial = fields.Char(string='Folio de pase de acceso de Seguridad Patrimonial')
+
+	observations= fields.Text(string='Observations')
+	deliver_pro = fields.Char(string="Lugar de enterga", help='Ingresa aqui el lugar de la entrega')
+	numor_customer = fields.Char(string='Numero de Pedido Cliente')
+	fianza = fields.Char(string='Fianza')
+	receipt_date = fields.Date(string='Fecha de recepcion')
+	delivery_time = fields.Char(string='Tiempo de Entrega')
+	made_by = fields.Many2one('res.users', string='Elaborado por')
+
+class FieldsAlbaran(models.Model):
+
+	_inherit = 'stock.picking'
+
+	observations = fields.Text(string='Observations')
+	op_text = fields.Char(string='OP')
+
+class ClabeField(models.Model):
+    
+	_inherit = 'res.partner'
+  
+	clabe_banco = fields.Char(string='CLABE', help='Aqui puedes ingresar tu CLABE de tu cuenta bancaria')
